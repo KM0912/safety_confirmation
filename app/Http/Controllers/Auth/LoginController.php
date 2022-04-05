44085687395e19efba59222d567599fb493564d9
KM0912 <61630275+KM0912@users.noreply.github.com>
2022-04-06 08:06:51 +0900

    ログイン中にログイン画面に遷移した場合のリダイレクトを設定 / ログイン後の遷移先を変更

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      * @var string
      */
     // protected $redirectTo = RouteServiceProvider::HOME;
-    protected $redirectTo = '/safety_status';
+    protected $redirectTo = '/safety_registration';
 
     /**
      * Create a new controller instance.
