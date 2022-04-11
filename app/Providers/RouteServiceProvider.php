14db431768186fb713b55e6d6ece51c56ad04116
KM0912 <61630275+KM0912@users.noreply.github.com>
2022-04-12 08:35:36 +0900

    ログイン後の遷移先を修正

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    // public const HOME = '/home';
+    public const HOME = '/safety_registration';
 
     /**
      * Define your route model bindings, pattern filters, etc.
