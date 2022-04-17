b480cc17c18f5cc41f5e946872c13acf41199277
KM0912 <61630275+KM0912@users.noreply.github.com>
2022-04-18 08:17:24 +0900

    レイアウト調整

--- resources/views/safety_registration.blade.php
+++ resources/views/safety_registration.blade.php
@@ -13,7 +13,7 @@
                     <form method='POST' action="/store">
                         @csrf
                         <div class="mb-3">
-                            <label class="form-label">自身の安否</label>
+                            <label class="form-label registration-lavel">自身の安否</label>
                             <input type="radio" class="btn-check" name="own_condition" id="own-condition-safety" autocomplete="off" value=1 checked>
                             <label class="btn btn-outline-primary" for="own-condition-safety">無事</label>
 
@@ -21,7 +21,7 @@
                             <label class="btn btn-outline-primary" for="own-condition-injured">負傷</label>
                         </div>
                         <div class="mb-3">
-                            <label for="commentTextarea1" class="form-label">家族の安否</label>
+                            <label class="form-label registration-lavel">家族の安否</label>
                             <input type="radio" class="btn-check" name="family_condition" id="family-condition-safety" autocomplete="off" value=1 checked>
                             <label class="btn btn-outline-primary" for="family-condition-safety">無事</label>
 
@@ -32,7 +32,7 @@
                             <label class="btn btn-outline-primary" for="family-condition-unconfirmed">未確認</label>
                         </div>
                         <div class="mb-3">
-                            <label for="commentTextarea1" class="form-label">出社可否</label>
+                            <label class="form-label registration-lavel">出社可否</label>
                             <input type="radio" class="btn-check" name="go_to_work" id="go-to-work-yes" autocomplete="off" value=1 checked>
                             <label class="btn btn-outline-primary" for="go-to-work-yes">可能</label>
 
@@ -40,8 +40,8 @@
                             <label class="btn btn-outline-primary" for="go-to-work-no">不可能</label>
                         </div>
                         <div class="mb-3">
-                            <label for="commentTextarea1" class="form-label">{{ __('Comment') }}</label>
-                            <textarea name="comment" class="form-control bg-white" id="commentTextarea1" rows="3"></textarea>
+                            <label for="commentTextarea" class="form-label">{{ __('Comment') }}</label>
+                            <textarea name="comment" class="form-control bg-white" id="commentTextarea" rows="3"></textarea>
                         </div>
                         <button class="w-100 btn btn-primary btn-lg" type="submit">{{ __('Report') }}</button>
                     </form>
